6e66e610905dbeb2e1a76bdc1d8970d3b5d142e7
Mario Porras <mariomst88@gmail.com>
2016-05-20 16:45:15 +0200

    Se ha puesto el boton lo0co

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void bMensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ESTOY HASTA EL PENE");
+        }
     }
 }
